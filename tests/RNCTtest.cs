4d5d808fa05f75ef9b9ad99028033b3de44bc001
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-28 01:30:12 -0400

    Make the RomanNumeralConverter a test fixture.

--- tests/RNCTtest.cs
+++ tests/RNCTtest.cs
@@ -5,22 +5,25 @@ namespace RomanNumerals.Test {
 
     [TestFixture]
     public class RNCTest {
+        private RomanNumeralConverter c;
+
+        [SetUp]
+        public void setUp() {
+            c = new RomanNumeralConverter();
+        }
 
         [Test]
         public void shouldReturnZeroWhenGivenBadInput() {
-            var c = new RomanNumeralConverter();
             Assert.AreEqual(0, c.convert("BAD"));
         }
 
         [Test]
         public void shouldReturn1WhenGivenI() {
-            var c = new RomanNumeralConverter();
             Assert.AreEqual(1, c.convert("I"));
         }
 
         [Test]
         public void shouldReturn2WhenGivenII() {
-            var c = new RomanNumeralConverter();
             Assert.AreEqual(2, c.convert("II"));
         }
     }
